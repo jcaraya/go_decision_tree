97833f4d7e9d4a0bc70ce59068e07ca85ae2b67b
Jorge Carvajal <jaraya6@gatech.edu>
2020-08-14 22:27:19 -0600

    Partition the dataset into training, validation and test
    Signed-off-by: Jorge Carvajal <jaraya6@gatech.edu>

--- cmd/main.go
+++ cmd/main.go
@@ -3,16 +3,41 @@ package main
 import (
 	"fmt"
 	"go_decision_tree/pkg/csvreader"
+	"go_decision_tree/pkg/dataset"
 	"go_decision_tree/pkg/parser"
+	"log"
 )
 
 func main() {
-	lines, err := csvreader.ReadCsv("./data/iris.data")
+	filePath := "./data/iris.data"
+	lines, err := csvreader.ReadCsv(filePath)
 	if err != nil {
-		panic(err)
+		log.Printf("failed reading file: %s", filePath)
+		return
 	}
 
 	x, y, classes, err := parser.ParseCsvLines(lines)
+	if err != nil {
+		log.Println("failed parsing the dataset lines with error: ", err)
+		return
+	}
+
+	fmt.Println("Len:", len(x))
+	x, y = dataset.ShuffleDataset(x, y)
+
+	// Partition the data
+	xTrain, yTrain := x[:90], y[:90]
+	_ = xTrain
+	_ = yTrain
+
+	xValidation, yValidation := x[90:120], y[90:120]
+	_ = xValidation
+	_ = yValidation
+
+	xTest, yTest := x[120:150], y[120:150]
+	_ = xTest
+	_ = yTest
+
 	fmt.Println(x[:10])
 	fmt.Println(y[:10])
 	fmt.Println(classes)
